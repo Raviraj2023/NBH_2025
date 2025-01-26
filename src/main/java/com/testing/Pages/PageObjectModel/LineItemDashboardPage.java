c105e459dac5bcef7737c244d070c53441fddc55
sagarlaxmanpanchal <panchalsagar2149@gmail.com>
2025-01-26 13:34:23 +0530

    added another testcases for line item dashboard

---
+++ src/main/java/com/testing/Pages/PageObjectModel/LineItemDashboardPage.java
@@ -0,0 +1,29 @@
+package com.testing.Pages.PageObjectModel;
+
+import com.testing.Base.CommonToAllPages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LineItemDashboardPage extends CommonToAllPages {
+    WebDriver driver;
+
+   public LineItemDashboardPage(WebDriver driver){
+        this.driver=driver;
+    }
+
+    private By Financial_Management=By.xpath("//div[contains(@class,'nb__DAYS7')]/div[text()='Financial Management']");
+   private By Billing=By.xpath("//div[contains(@class,'nb__DAYS7') and text()='Billing']");
+   private By Line_item_dashboard=By.xpath("//div[contains(@class,'nb__DAYS7') and text()='Line item dashboard']");
+   private By Line_item_dashboard_title=By.xpath("//div[contains(@class,'nb__c7kE9') and text()='Line Item Dashboard']");
+
+
+   public String verifyLineItemDashboardPage(){
+       System.out.println("test");
+
+       clickElement(Financial_Management);
+       clickElement(Billing);
+       clickElement(Line_item_dashboard);
+       return getText(Line_item_dashboard_title);
+
+   }
+}
---
+++ src/test/java/com/testing/Tests/LineItemDashboardTests.java
@@ -0,0 +1,10 @@
+package com.testing.Tests;
+
+import com.testing.Base.CommonToTestcases;
+import com.testing.Pages.PageObjectModel.LoginPage;
+import org.openqa.selenium.WebDriver;
+
+public class LineItemDashboardTests extends CommonToTestcases {
+    WebDriver driver;
+
+}
