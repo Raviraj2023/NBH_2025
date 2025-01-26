332b0fa528a66715f55ec92dc44b4c93d36962bb
sagarlaxmanpanchal <panchalsagar2149@gmail.com>
2025-01-26 22:08:35 +0530

    added second Test case with custome logs

--- src/main/java/com/testing/Pages/PageObjectModel/LineItemDashboardPage.java
+++ src/main/java/com/testing/Pages/PageObjectModel/LineItemDashboardPage.java
@@ -2,27 +2,44 @@ package com.testing.Pages.PageObjectModel;
 
 import com.testing.Base.CommonToAllPages;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 
 public class LineItemDashboardPage extends CommonToAllPages {
     WebDriver driver;
 
-   public LineItemDashboardPage(WebDriver driver){
-        this.driver=driver;
+    public LineItemDashboardPage(WebDriver driver) {
+        this.driver = driver;
     }
+    private By Financial_Management = By.xpath("//div[contains(@class,'nb__DAYS7')]/div[text()='Financial Management']");
+    private By Billing = By.xpath("//div[contains(@class,'nb__DAYS7') and text()='Billing']");
+    private By Line_item_dashboard = By.xpath("//div[contains(@class,'nb__DAYS7') and text()='Line item dashboard']");
+    private By Line_item_dashboard_title = By.xpath("//div[contains(@class,'nb__c7kE9') and text()='Line Item Dashboard']");
 
-    private By Financial_Management=By.xpath("//div[contains(@class,'nb__DAYS7')]/div[text()='Financial Management']");
-   private By Billing=By.xpath("//div[contains(@class,'nb__DAYS7') and text()='Billing']");
-   private By Line_item_dashboard=By.xpath("//div[contains(@class,'nb__DAYS7') and text()='Line item dashboard']");
-   private By Line_item_dashboard_title=By.xpath("//div[contains(@class,'nb__c7kE9') and text()='Line Item Dashboard']");
+    public String verifyLineItemDashboardPage() {
+        logger.info("Starting verification of Line Item Dashboard Page");
 
-
-   public String verifyLineItemDashboardPage(){
-       System.out.println("hello ravi");
-       clickElement(Financial_Management);
-       clickElement(Billing);
-       clickElement(Line_item_dashboard);
-       return getText(Line_item_dashboard_title);
-
-   }
+        try {
+            logger.info("Clicking on Financial Management menu");
+            clickElement(Financial_Management);
+            logger.info("Clicked on Financial Management menu");
+            logger.info("Clicking on Billing menu");
+            clickElement(Billing);
+            logger.info("Clicked on Billing menu");
+            logger.info("Clicking on Line Item Dashboard");
+            clickElement(Line_item_dashboard);
+            logger.info("Clicked on Line Item Dashboard");
+        } catch (Exception e) {
+            logger.error("Error while navigating to Line Item Dashboard: " + e.getMessage(), e);
+        }
+        logger.info("waiting for Line_item_dashboard_title to be visible ");
+        try {
+            visiblityOfeElement(Line_item_dashboard_title);
+        } catch (TimeoutException e) {
+            logger.error("Timeout while waiting for an element: " + e.getMessage(), e);
+        }
+        String title = getText(Line_item_dashboard_title);
+        logger.info("Retrieved Line Item Dashboard title: " + title);
+        return title;
+    }
 }
--- src/test/java/com/testing/Base/CommonToTestcases.java
+++ src/test/java/com/testing/Base/CommonToTestcases.java
@@ -1,13 +1,16 @@
 package com.testing.Base;
 
 import com.testing.Driver.DriverManager;
+import com.testing.Pages.PageObjectModel.LoginPage;
 import com.testing.Utils.PropertyReader;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 import org.apache.log4j.*;
 
 public class CommonToTestcases {
+    WebDriver driver;
     @BeforeTest
     public void setup(){
         DriverManager.initialise(PropertyReader.readKey("browser"));
@@ -19,4 +22,9 @@ public class CommonToTestcases {
     }
 
     public Logger logger=Logger.getLogger(getClass().getSimpleName());
+
+    public void login (){
+        LoginPage lg = new LoginPage(driver);
+        lg.loginWithValidCredentials();
+    }
 }
